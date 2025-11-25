ec264ea456a1753b3b7f4c9220b07923f53fd985
Spacecer2 <Fabian_hoess@outlook.de>
2025-11-25 16:43:44 +0100

    refactor: make portfolio container responsive and remove unused elements

--- run_app.sh
+++ run_app.sh
@@ -2,6 +2,11 @@
 echo "Activating virtual environment..."
 source .venv/bin/activate
 echo "Virtual environment activated."
+pip install -r requirements.txt
+echo "Dependencies installed."
+echo "Upgrading pip..."
+pip install --upgrade pip
+echo "Pip upgraded."
 echo "Running the app.py script..."
 python app.py
 echo "Script finished."
